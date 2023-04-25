cddc3e7e1eea032bca5ee0de9d06f2609d64f17f
yann <yannlefranc330@gmail.com>
2023-04-25 11:00:22 +0200

    Class et boucle

---
+++ src/thewitcher.java
@@ -0,0 +1,37 @@
+public class thewitcher {
+    public static void main( String [] args ) {
+
+        boolean debug = true;
+
+        System.out.println( "the witcher" );
+        if ( debug ) {
+            System.out.println( "I have my perssonality" ) ;
+        }
+
+        int i =10;
+        while( i < 0 ) {
+            System.out.println( "i == " + i );
+            i--;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
